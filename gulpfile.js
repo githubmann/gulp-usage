54a4f502479445ad8968eba15dddde9855c6d0a9
githubmanm <1041763635@qq.com>
2016-02-01 23:59:08 +0800

    gulp.watch的使用
    gulp.watch(glob[, opts, cb])
    
    gulp.watch(glob[, opts], tasks)
    glob是文件匹配方式

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@
 * @Author: 波
 * @Date:   2016-02-01 22:34:24
 * @Last Modified by:   波
-* @Last Modified time: 2016-02-01 23:36:00
+* @Last Modified time: 2016-02-01 23:54:49
 */
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browser = require('browser-sync');
+var browserSync = require('browser-sync');
 //sass静态任务
 //你的任务是否在这些前置依赖的任务完成之前运行了？
 //请一定要确保你所依赖的任务列表中的任务都使用了正确的异步执行方式：
@@ -15,17 +15,21 @@ var browser = require('browser-sync');
 gulp.task('sass',function  () {
   return gulp.src('./scss/*.scss')
       .pipe(sass())
+      .pipe(browserSync.reload({stream:true}))
       .pipe(gulp.dest('./css/'));//如果css不存在会自动创建
 })
 //browser静态任务
-gulp.task('browser',['sass'],function  () {
-  browser({
+gulp.task('browserSync',['sass'],function  () {
+  browserSync({
     server:{
       baseDir:'./'
     }
   })
 })
 gulp.task('watch',function  () {
-  gulp.watch(['scss/*.scss','*.html'],['sass']);
-});
-gulp.task('default',['browser','watch']);
+  gulp.watch('scss/*.scss',['sass']);
+  gulp.watch('*.html',function  () {
+    browserSync.reload();
+  })
+})  
+gulp.task('default',['browserSync','watch']);
